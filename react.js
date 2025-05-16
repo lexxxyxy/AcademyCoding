b70afdfaf2f57b6760cc79fb4fcbac0126f9664d
Lexxxy11 <165573445+lexxxyxy@users.noreply.github.com>
2025-05-16 20:52:10 +0700

    Add files via upload

--- react.js
+++ react.js
@@ -1,18 +1,29 @@
 function subscribe(plan) {
-    alert(`Anda telah memilih paket ${plan}. Terima kasih atas langganan Anda!`);
-  }
-  
-  function contact() {
-    alert("Silakan hubungi kami di email academy@coding.com atau melalui WhatsApp.");
-  }
+  alert(`Anda telah memilih paket ${plan}. Terima kasih atas langganan Anda!`);
+}
 
-      // Toggle details open/close icon rotation
-      document.querySelectorAll("details").forEach((detail) => {
-        detail.addEventListener("toggle", (e) => {
-          if (detail.open) {
-            detail.querySelector("summary i").classList.add("rotate-180");
-          } else {
-            detail.querySelector("summary i").classList.remove("rotate-180");
-          }
-        });
-      });
+function contact() {
+  alert(
+    "Silakan hubungi kami di email academy@coding.com atau melalui WhatsApp."
+  );
+}
+
+// Toggle details open/close icon rotation
+document.querySelectorAll("details").forEach((detail) => {
+  detail.addEventListener("toggle", (e) => {
+    if (detail.open) {
+      detail.querySelector("summary i").classList.add("rotate-180");
+    } else {
+      detail.querySelector("summary i").classList.remove("rotate-180");
+    }
+  });
+});
+
+AOS.init({
+  once: true,
+  duration: 800,
+  easing: "ease-in-out",
+});
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({ duration: 1000 });
+});
